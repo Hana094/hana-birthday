f5f80c1d18171ac34f3cebe775f4085c948dd3bf
hana094 <hanavc094@gmail.com>
2024-09-20 14:52:34 -0600

    final fix

--- src/pages/virtual.jsx
+++ src/pages/virtual.jsx
@@ -10,13 +10,16 @@ const Virtual = () => {
     <div className="main-container">
       <div className="secondary-container">
         <div className="glowing-secondary">"Hana, I can't make it"</div>
-        <div className="normal">No worries sugar!</div>
+        <div className="normal">No worries, sugar!</div>
         <div className="secondary-text">
-          You can always be part of the remote celabration!
+          You can always be part of the remote celebration!
         </div>
         <div className="secondary-text">
-          It will take place in dst, join us and the kawaiijus to an intense
-          session of dst, we will be wating over our discord
+          It will take place on DST, join the kawaiijus and I in an intense game
+          of Don't Starve together.
+        </div>
+        <div className="secondary-text">
+          We'll be waiting for you on our Discord.
         </div>
         <div className="outrun">One of us</div>
         <button className="glass-button" onClick={handleClick}>
